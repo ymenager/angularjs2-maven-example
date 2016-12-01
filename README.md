49504f0fe6fe41dd4cdcb54f424e37df6741347a
Yannick Menager <ymenager@kloudtek.com>
2016-11-30 18:55:06 -0800

    Initial import

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # angularjs2-maven-example
+
+In order to build, the user must have npm and tsc installed
+ 
+MAC (brew):
+ 
+brew install npm
+npm install -g typescript
---
+++ src/main/angularjs/app/app.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+	templateUrl: 'app/app.component.html',
+	selector: 'my-app'
+})
+export class AppComponent {
+}
